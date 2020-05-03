0a0c38198a785e7d1c549238863c99db31faa566
Quanthon <18911290852@163.com>
2020-05-04 03:32:48 +0800

    增加了设置界面的UI

---
+++ iyingdiBattleGround/Assets/Scripts/UI/FocusShow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class FocusShow : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject ShowingGo;
+
+    private void Awake()
+    {
+        ShowingGo.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ShowingGo.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ShowingGo.SetActive(false);
+    }
+}
---
+++ iyingdiBattleGround/Assets/Scripts/UI/Panel/OptionPanel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionPanel : UIPanel
+{
+    
+    public void ResolutionChanged(int index)
+    {
+
+    }
+
+    public void ClickMusicButton()
+    {
+
+    }
+
+    public void ClickSoundButton()
+    {
+
+    }
+
+    public void AdjustMusicValue(float value)
+    {
+
+    }
+
+    public void AdjustSoundValue(float value)
+    {
+
+    }
+
+    public void ClickConfirm()
+    {
+
+    }
+
+    public void ClickBackToMain()
+    {
+
+    }
+
+    public void ClickExitGame()
+    {
+
+    }
+
+
+}
